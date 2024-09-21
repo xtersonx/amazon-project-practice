9c85cbd707f60312691e14019bdc8f7b292d22ee
abdel rahman <balmrdyny@gmail.com>
2024-09-22 00:38:36 +0300

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,21 @@
-export let cart =JSON.parse(localStorage.getItem('cart')) || [
-  {
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 1,
-    deliveryOptionId: '1'
-  },
-  {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }
-];
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart =JSON.parse(localStorage.getItem('cart')) || [
+    {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1,
+      deliveryOptionId: '1'
+    },
+    {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }
+  ];
+}
 
 export function addToCart(productId, quantitySelector) {
   let isFound = false;
@@ -31,7 +37,6 @@ export function addToCart(productId, quantitySelector) {
     });
   }
   saveToStorage();
-  console.log(cart);
 }
 
 
